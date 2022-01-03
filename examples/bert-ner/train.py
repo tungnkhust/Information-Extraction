401c74fdb4c3685f08f8b1e60a2f700670f4c78e
VikingTC <nguyenkytungcntt04@gmail.com>
2022-01-03 22:21:44 +0700

    add bert ner

--- examples/bert-ner/train.py
+++ examples/bert-ner/train.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     parser.add_argument("-dev", "--dev_path", default="data/CoNLL04/dev.txt")
     parser.add_argument("-test", "--test_path", default="data/CoNLL04/test.txt")
     parser.add_argument("-out", "--output_dir", default="./models/bert-ner")
-    parser.add_argument("-bs", "--batch_size", type=int, default=2)
-    parser.add_argument("-epoch", "--num_epochs", type=int, default=2)
+    parser.add_argument("-bs", "--batch_size", type=int, default=64)
+    parser.add_argument("-epoch", "--num_epochs", type=int, default=10)
     parser.add_argument("-wd", "--weight_decay", type=float, default=0.01)
     parser.add_argument("-lr", "--learning_rate", type=float, default=0.01)
     parser.add_argument("--max_grad_norm", type=int, default=1.0)
@@ -53,16 +53,16 @@ if __name__ == '__main__':
         label2idx=label2idx
     )
 
-    # ner.train(
-    #     train_dataset=train_dataset,
-    #     eval_dataset=dev_dataset,
-    #     num_epochs=args.num_epochs,
-    #     batch_size=args.batch_size,
-    #     output_dir=args.output_dir,
-    #     weight_decay=args.weight_decay,
-    #     learning_rate=args.learning_rate,
-    #     save_strategy=args.save_strategy,
-    #     evaluation_strategy=args.evaluation_strategy,
-    #     max_grad_norm=args.max_grad_norm,
-    #     fp16=args.fp16
-    # )
+    ner.train(
+        train_dataset=train_dataset,
+        eval_dataset=dev_dataset,
+        num_epochs=args.num_epochs,
+        batch_size=args.batch_size,
+        output_dir=args.output_dir,
+        weight_decay=args.weight_decay,
+        learning_rate=args.learning_rate,
+        save_strategy=args.save_strategy,
+        evaluation_strategy=args.evaluation_strategy,
+        max_grad_norm=args.max_grad_norm,
+        fp16=args.fp16
+    )
